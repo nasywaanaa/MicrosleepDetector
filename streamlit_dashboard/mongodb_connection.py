a3350747077795b836f62c04f5b94b2940861c7d
Jihan <jihanaurelia2004@gmail.com>
2025-04-17 21:45:08 +0700

    deploy streamlit

--- streamlit_dashboard/mongodb_connection.py
+++ streamlit_dashboard/mongodb_connection.py
@@ -1,8 +1,11 @@
 import pymongo
 import streamlit as st
 
+# Load kredensial dari secrets
+sec = st.secrets["secrets"]
+
 def get_mongo_client():
-    MONGO_URI = st.secrets["MONGO_URI"]
+    MONGO_URI = sec["MONGO_URI"]
     
     if not MONGO_URI:
         raise ValueError("MONGO_URI is not set in secrets")
